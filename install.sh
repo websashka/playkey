b5b05905254b0d8a54ff059c1b9a2ba84f14c381
Alexander <ondepopov@yandex.ru>
2022-03-19 16:28:09 +0300

    Install script

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+echo "Install...";
+cp -r ./bin ~/playkey/;
+cp playkey.desktop ~/.local/share/applications/;
+PATH=$PATH:~/playkey/bin;
+xdg-mime default playkey.desktop x-scheme-handler/playkey;
+echo "Done.";
